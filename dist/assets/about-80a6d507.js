3dd52167c283bcb8a305a8b09b2615f96fcbb280
Leandro Matías Gómez <lmgomez@DH.digitalhouse.com>
2023-01-19 18:47:56 -0300

    a ver ahora

--- dist/assets/about-80a6d507.js
+++ dist/assets/about-80a6d507.js
@@ -1,6 +1,6 @@
-import"./modulepreload-polyfill-ec808ebb.js";document.querySelector("#app").innerHTML=`
+import{v as e,s as t}from"./viteLogo-f2d5c276.js";document.querySelector("#app").innerHTML=`
   <div>
- 
+    <img src='${e}' alt='viteLogo'
     <h1>Calculadora1</h1>
     <input type="text" id="numero1" placeholder="inserte numero 1">
     <input type="text" id="numero2" placeholder="inserte numero 2">
@@ -10,4 +10,4 @@ import"./modulepreload-polyfill-ec808ebb.js";document.querySelector("#app").inne
     </div>
     <p id="showRes"></p>
   </div>
-`;document.getElementById("btn").addEventListener("click",()=>{showResult(document.querySelector("#showRes"),document.getElementById("numero1").value,document.getElementById("numero2").value,document.getElementById("operacion").value)});
+`;document.getElementById("btn").addEventListener("click",()=>{t(document.querySelector("#showRes"),document.getElementById("numero1").value,document.getElementById("numero2").value,document.getElementById("operacion").value)});
